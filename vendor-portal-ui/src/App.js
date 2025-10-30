6d73f18c296bb91dcc4a761c4929e9ed0f12dc6b
Kushagra Agarwal <kushagraagarwal2003@gmail.com>
2025-10-30 21:14:28 +0530

    feat: Update routing in App.js to link to individual pages and add .gitignore file

--- vendor-portal-ui/src/App.js
+++ vendor-portal-ui/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VendorDetails from './components/VendorDetails';
 import './App.css';
-import Home from './pages/Home';
+import Home from './pages/home';
+import AboutUs from './pages/AboutUs';
+import OurBusiness from './pages/OurBusiness';
+import OurImpact from './pages/OurImpact';
+import OurValues from './pages/OurValues';
 
 function App() {
   return (
@@ -10,10 +14,10 @@ function App() {
         {/* <h1>Vendor Portal Application</h1> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about_us" element={<Home />} />
-          <Route path="/our_business" element={<Home />} />
-          <Route path="/our_impact" element={<Home />} />
-          <Route path="/our_values" element={<Home />} />
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/our_business" element={<OurBusiness />} />
+          <Route path="/our_impact" element={<OurImpact />} />
+          <Route path="/our_values" element={<OurValues />} />
           <Route path="/vendor/:id" element={<VendorDetails />} />
         </Routes>
       </div>
